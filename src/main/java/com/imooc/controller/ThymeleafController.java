813d05eee38043f08d65abf57131ab110bd78015
GZ-1 <275152725@qq.com>
2018-05-09 12:21:49 -0700

    index网页失败

--- src/main/java/com/imooc/controller/ThymeleafController.java
+++ src/main/java/com/imooc/controller/ThymeleafController.java
@@ -1,16 +1,20 @@
 package com.imooc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+//@ResponseBody
 @RequestMapping("/")
 public class ThymeleafController {
 
     @RequestMapping("/index")
-    public String index(ModelMap map){
-        map.addAttribute("name","thymeleaf-imooc");
+    public String index(ModelMap map) {
+        map.addAttribute("name","thymeleaf-imho");
         return "thymeleaf/index";
     }
 
